51b69513e4b1930d760222c5e923904e6559c51d
DyatlovIvan <diatlovivan92@mail.ru>
2021-10-13 15:23:30 +0400

    test2

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -19,6 +21,7 @@ function App() {
           Learn React
         </a>
         <p>1 2 3</p>
+        <p>1</p>
       </header>
     </div>
   );
