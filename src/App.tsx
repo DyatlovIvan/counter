ec813ead77295c4dcf8bf9828c5dc58c8241b7ba
DyatlovIvan <diatlovivan92@mail.ru>
2021-10-18 17:27:27 +0400

    add mew functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,25 @@ import './App.css';
 import {Counter} from "./counter";
 
 function App() {
-    let [counter,setCounter] = useState(0)
+    let [counter,setCounter] = useState<number|string>(5)
 
-    const incCounter = (value:number)=>setCounter(++counter)
+    const incCounter = (value:number| string)=>{
+      if( typeof counter === 'number' ){
+      setCounter(++counter)
+      }
+    }
 
-    const restCounter = (value:number)=>setCounter(0)
+    const restCounter = (value:number| string)=>setCounter(value)
+
+    const setTitle = (value:number| string)=> setCounter(value)
 
   return (
     <div className="App">
       <Counter
           counter = {counter}
           incCounter = {incCounter}
-          restCounter = {restCounter}/>
+          restCounter = {restCounter}
+          setTitle = {setTitle}/>
     </div>
   );
 }
--- src/counter.tsx
+++ src/counter.tsx
@@ -1,33 +1,88 @@
 import style from './counter.module.css'
-import {Button} from "./Button";
+import { Button } from "./Button";
+import { ChangeEvent, useState } from 'react';
 
 
 type CounterType = {
-    counter: number
-    incCounter: (value: number) => void
-    restCounter: (value: number) => void
+    counter: number | string
+    incCounter: (value: number | string) => void
+    restCounter: (value: number | string) => void
+    setTitle: (value: number | string) => void
 }
 
-export const Counter = ({counter, incCounter, restCounter, ...props}: CounterType) => {
+export const Counter = ({ counter, incCounter, restCounter, ...props }: CounterType) => {
+    let [maxValue, setMaxValue] = useState(5)
+    let [minValue, setMinValue] = useState(0)
+    const setMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
 
-    const incHandler = () => incCounter(counter)
-    const restHandler = () => restCounter(counter)
+        setMaxValue(JSON.parse(e.currentTarget.value))
 
-    const disabledIncHandler = () => counter === 5 ? true : false
-    const disabledRestHandler = () => counter===0?true:false
+        minValue===maxValue?props.setTitle('Incorrect value!'):props.setTitle('enter values and press "set"')
+
+    }
+    const setMinValueHandler = (e: ChangeEvent<HTMLInputElement>) =>{
+        
+         setMinValue(JSON.parse(e.currentTarget.value))
+
+
+
+    }
+    const setHandler = () => {
+        props.setTitle(minValue)
+    }
+
+    const setDisabledHandler = minValue>=maxValue?true:false
+
+
+
+
+
+
+    const incHandler = () => incCounter(minValue)
+    const restHandler = () => restCounter(minValue)
+
+    const disabledIncHandler = () => counter === maxValue ? true : false
+    const disabledRestHandler = () => counter === minValue ? true : false
 
     return (
-        <div className={style.counter}>
-            <input className={`${counter === 5 ? style.limit : ''} ${style.input}`} value={counter}/>
-            <div className={style.buttonBox}>
-                <Button callBack={incHandler}
+        <div className={style.all}>
+            <div className={style.counter}>
+                <div>
+                    max value:
+                    <input value={maxValue} type='number' onChange={setMaxValueHandler} 
+                    className={minValue===maxValue?style.inputError:''}/>
+                </div>
+                <div>
+                    min value:
+                    <input value={minValue} type='number' onChange={setMinValueHandler}
+                    className={minValue>=maxValue?style.inputError:''}/>
+                </div>
+                <div>
+                    <Button callBack={setHandler}
+                        name='set'
+                        disabled={setDisabledHandler} />
+                </div>
+
+            </div>
+
+
+
+
+
+
+
+            <div className={style.counter}>
+                <input className={`${counter === maxValue ? style.limit : ''} ${style.input}`} value={counter} />
+                <div className={style.buttonBox}>
+                    <Button callBack={incHandler}
                         name='inc'
                         disabled={disabledIncHandler()}
-                />
-                <Button callBack={restHandler}
+                    />
+                    <Button callBack={restHandler}
                         name='rest'
                         disabled={disabledRestHandler()}
-                />
+                    />
+                </div>
             </div>
         </div>
     )
