d7a0d52690d4a2e1536aeddee570852cac88f65d
DyatlovIvan <diatlovivan92@mail.ru>
2021-11-13 17:56:26 +0400

    redux counter

--- src/bll/store.ts
+++ src/bll/store.ts
@@ -1,11 +1,22 @@
 import {combineReducers, createStore} from "redux";
 import {counterReducer} from "./counter-reducer";
+import {loadState, saveState} from "../utils/localstorage-utils";
 
 
 const rootReducer = combineReducers({
     counter: counterReducer
 })
-export type StateType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer)
+
+
+
+export const store = createStore(rootReducer,loadState())
+
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    });
+})
+
+export type StateType = ReturnType<typeof rootReducer>
 export const StoreType = typeof store;
